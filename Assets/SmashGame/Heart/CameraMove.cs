0fdabfe368f182e38e619b5065ece6cba67be13c
Ivanstarterprog <ivan.nurgazin@yandex.ru>
2024-04-11 00:31:46 +0700

    Добавление Функциональности игры
    Добавление скрипта на возвращение сердца в начало уровня
    Добавление пуль-сердец
    Добавление скрипта на стрельбу
    Добавление скрипта на автоматическое удаление пуль после 4 секунд жизни

--- Assets/SmashGame/Heart/CameraMove.cs
+++ Assets/SmashGame/Heart/CameraMove.cs
@@ -1,19 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEditor.UI;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraMove : MonoBehaviour
 {
     public float speed = 1.0f;
-    float time;
+    public float pushForce;
+    public float time;
+    Vector3 startPosition;
+    public GameObject shootingPoint;
+    public GameObject Bullet;
+    public GameObject bulletScript;
     void Start()
     {
+        startPosition = transform.position;
         time = Time.deltaTime;
     }
 
-    // Update is called once per frame
+    void OnTriggerEnter(Collider otherObject)
+
+    {
+
+        BackToSpawn();
+
+    }
+
+    void BackToSpawn()
+    {
+        transform.position = startPosition;
+    }
+
+
+
     void Update()
     {
         gameObject.transform.Translate(Vector3.forward * speed * time);
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject bullet = Instantiate(Bullet, shootingPoint.transform.position, transform.rotation);
+                Vector3 dir = ray.origin - hit.point;
+                dir.Normalize();
+                bullet.GetComponent<Rigidbody>().AddForce(-1 * dir * pushForce, ForceMode.Impulse);
+            }
+        }
     }
 }
