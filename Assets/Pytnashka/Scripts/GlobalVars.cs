da5fdb0d6978d12c1db79d2b57fb44615d51989e
Станислав Хозяенок <st.hoz.kem@gmail.com>
2024-04-14 19:59:58 +0700

    Updated

--- Assets/Pytnashka/Scripts/GlobalVars.cs
+++ Assets/Pytnashka/Scripts/GlobalVars.cs
@@ -7,14 +7,5 @@ public class GlobalVars : MonoBehaviour
     // Start is called before the first frame update
     public static float x_offset = 1.0f;
     public static float z_offset = 1.0f;
-    //void Start()
-    //{
-        
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-        
-    //}
+    public static GameObject[] chips;
 }
