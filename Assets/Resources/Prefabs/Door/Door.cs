ea33841ec039368e0c22fbfa380db5b40f2f5348
Hamjoshua <biryukov-10@list.ru>
2024-04-14 19:13:39 +0700

    Добавлена дверь с открыванием

---
+++ Assets/Resources/Prefabs/Door/Door.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{    
+    [SerializeField]
+    private bool _isOpened = false;
+    public float OpenAngle = -120f;
+    public bool OpenedOnStart = false;
+    public float OpeningSpeed = 100f;
+
+    //public void Update()
+    //{
+    //    OpenDoor();
+    //}
+
+    public void Awake()
+    {
+        OpenDoor();
+        if (OpenedOnStart)
+        {
+            SetToHingeJointTarget(OpenAngle, true);
+        }
+    }
+
+    public void OpenDoor()
+    {
+        SetToHingeJointTarget(OpenAngle);
+    }
+
+    public void CloseDoor()
+    {
+        SetToHingeJointTarget(0f);
+    }
+
+    private void SetToHingeJointTarget(float targetPosition, bool fast = false)
+    {
+        HingeJoint hingeJoint = transform.GetChild(1).GetComponent<HingeJoint>();
+        JointSpring jointSpring = hingeJoint.spring;
+        if (fast)
+        {
+            jointSpring.spring = 10000;
+        }
+        else
+        {
+            jointSpring.spring = OpeningSpeed;
+        }
+        
+        jointSpring.targetPosition = targetPosition;
+        hingeJoint.spring = jointSpring;
+    }    
+}
